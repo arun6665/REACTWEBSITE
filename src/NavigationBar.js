61edc4445bb79c437159d93fe0a410931dd25901
arun6665 <tigermhs77@gmail.com>
2021-03-12 08:25:12 +0545

    nav bar

---
+++ src/NavigationBar.js
@@ -0,0 +1,26 @@
+import { Container, Typography }   from '@material-ui/core';
+import React           from 'react';
+import Toolbar         from '@material-ui/core/Toolbar';
+
+
+
+
+export default function NavigationBar()
+{
+return (
+<Container>
+
+  <Toolbar>
+
+      <Typography>
+         Roy Company
+      </Typography>
+
+  </Toolbar>
+
+</Container>
+
+);
+
+
+}
