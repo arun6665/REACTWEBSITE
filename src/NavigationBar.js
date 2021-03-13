7411b34f396a8e6307d30f69f50593520bd9705f
arun6665 <tigermhs77@gmail.com>
2021-03-13 09:31:23 +0545

    navbar completed

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,20 +1,65 @@
-import { Container, Typography }   from '@material-ui/core';
+import { Container, Typography, Box, Link}   from '@material-ui/core';
 import React           from 'react';
 import Toolbar         from '@material-ui/core/Toolbar';
+import { makeStyles }   from '@material-ui/core/styles';
 
 
+const useStyles = makeStyles((theme) => ({  
 
+siteTitle: {
+fontWeight: 'bold',
+letterSpacing: 1.5
+},
+  
+  toolbar: {
+   display: 'flex',
+   flexDirection: 'column',
+   [theme.breakpoints.up('md')]:{
+     flexDirection: 'row',
+  
+     justifyContent: 'space-between'
+   }
+  },
 
+  menuBox: {
+   display: "flex",
+   flexDirection: "column",
+   [theme.breakpoints.up('md')]: {
+     flexDirection: 'row'
+   }
+
+  },
+   menuOption: {
+     padding: theme.spacing(1),
+     [theme.breakpoints.up('md')]:{
+       paddingLeft: theme.spacing(3)
+     }
+   }
+}));
 export default function NavigationBar()
 {
+  const classes = useStyles();
 return (
 <Container>
 
-  <Toolbar>
+  <Toolbar className ={classes.toolbar}>
 
-      <Typography>
-         Roy Company
+      <Typography component='h1' variant='h4' className={classes.siteTitle}>
+         Course Nepal
       </Typography>
+
+      <Box className={classes.menuBox}>
+      {['home', 'courses', 'sign up'].map((menuOption) => (
+        <Link
+         component='button'
+         variant='body1'
+         className={classes.menuOption}
+        >
+         {menuOption.toUpperCase()}
+         </Link>
+    ))}
+
+      </Box>
 
   </Toolbar>
 
